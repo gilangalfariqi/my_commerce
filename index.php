11719cc896d1d5953f02dedc9535d0a832c5bc9b
Gilang Alfariqi <gilangalfariqi@gmail.com>
2026-05-30 02:47:19 +0700

    fix database

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$uri = "mysql://avnadmin:<redacted>@mysql-1b43c5d0-gilangalfariqi-bengkel.l.aivencloud.com:26214/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=defaultdb";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try {
+  $db = new PDO($conn, $fields["user"], $fields["pass"]);
+
+  $stmt = $db->query("SELECT VERSION()");
+  print($stmt->fetch()[0]);
+} catch (Exception $e) {
+  echo "Error: " . $e->getMessage();
+}
