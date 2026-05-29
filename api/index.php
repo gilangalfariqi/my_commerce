06a32e6f434d72f693d10dfc47768aedd0cbd28d
Gilang Alfariqi <gilangalfariqi@gmail.com>
2026-05-30 02:20:23 +0700

    fix: redirect all bootstrap cache files to writeable /tmp directory

--- api/index.php
+++ api/index.php
@@ -9,24 +9,33 @@ define('LARAVEL_START', microtime(true));
 require __DIR__.'/../vendor/autoload.php';
 
 try {
-    // 2. Bootstrap Laravel
-    /** @var Application $app */
-    $app = require_once __DIR__.'/../bootstrap/app.php';
-
-    // 3. Customize storage path for Vercel writeable /tmp directory
+    // 2. Customize storage & bootstrap cache paths for Vercel writeable /tmp directory
     $vercelStorage = '/tmp/storage/framework';
-    foreach (['views', 'cache', 'sessions'] as $dir) {
-        $path = $vercelStorage . '/' . $dir;
+    $vercelCache = '/tmp/storage/bootstrap/cache';
+
+    // Ensure all necessary directories exist in writeable /tmp
+    foreach ([$vercelStorage . '/views', $vercelStorage . '/cache', $vercelStorage . '/sessions', $vercelCache] as $path) {
         if (!is_dir($path)) {
             mkdir($path, 0755, true);
         }
     }
 
-    // Set paths & drivers for Vercel
+    // Override Laravel default storage paths and drivers
     putenv('VIEW_COMPILED_PATH=' . $vercelStorage . '/views');
-    putenv('SESSION_DRIVER=cookie');
-    putenv('CACHE_STORE=array');
-    putenv('LOG_CHANNEL=stderr');
+    putenv('SESSION_DRIVER=cookie'); // Serverless should use client-side cookies or DB sessions
+    putenv('CACHE_STORE=array');     // Avoid writing file cache to read-only directory
+    putenv('LOG_CHANNEL=stderr');    // Redirect logs to Vercel dashboard console
+
+    // Redirect all bootstrap cache files to writeable /tmp directory to bypass read-only filesystem limit
+    putenv('APP_SERVICES_CACHE=' . $vercelCache . '/services.php');
+    putenv('APP_PACKAGES_CACHE=' . $vercelCache . '/packages.php');
+    putenv('APP_CONFIG_CACHE=' . $vercelCache . '/config.php');
+    putenv('APP_ROUTES_CACHE=' . $vercelCache . '/routes.php');
+    putenv('APP_EVENTS_CACHE=' . $vercelCache . '/events.php');
+
+    // 3. Bootstrap Laravel
+    /** @var Application $app */
+    $app = require_once __DIR__.'/../bootstrap/app.php';
 
     // 4. Handle incoming HTTP request
     $app->handleRequest(Request::capture());
